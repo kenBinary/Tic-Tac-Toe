4ee31eec8421517914987e226a8d05067458a452
thennek0 <kennethbecaro0@gmail.com>
2022-04-17 19:17:57 +0800

    added horizontal checker for winning

--- script.js
+++ script.js
@@ -55,18 +55,50 @@ const game = (() => {
         const secondPlayer = playerTwo
         players.push(firstPlayer,secondPlayer);
     }
-    const playGame = (takeInput)=>{
+
+    const horizontalChecker = (board)=>{
+        const a = [...board];
+        const rows = {
+            first: a.slice(0,3),
+            second: a.slice(3,6),
+            third: a.slice(6,9)
+        }
+        if (rows.first.every(test) || rows.first.every(test2)) {
+            console.log("win first row")
+        }
+        else if(rows.second.every(test) || rows.second.every(test2)){
+            console.log("win second row")
+
+        }
+        else if(rows.third.every(test) || rows.third.every(test2)){
+            console.log("win third row")
+        }
+        else{
+            console.log("no winner")
+        }
+
+        function test(value) {
+            return value === "x";
+        }
+        function test2(value) {
+            return value === "o";
+        }
+    }
+    
+    const playGame = (takeInput,board)=>{
         const cell = document.querySelectorAll(".cell");
         cell.forEach((element)=>{
             element.addEventListener('click',(e)=>{
                 if ((e.target.textContent)==="") {
                     if (!players[0].isTurn) {
                         takeInput(element.id,players[0].choice)
+                        horizontalChecker(board);
                         players[0].isTurn = !players[0].isTurn
                         players[1].isTurn = !players[1].isTurn
                     }
                     else if (players[1].isTurn) {
                         takeInput(element.id,players[1].choice)
+                        horizontalChecker(board);
                         players[0].isTurn = !players[0].isTurn
                         players[1].isTurn = !players[1].isTurn
                     }
@@ -83,11 +115,12 @@ const game = (() => {
     return{
         playGame,
         initializePlayers,
-        players
+        players,
+        horizontalChecker
     }
 
 })();
 const first = player("test","x");
 const second = player("test2","o");
 game.initializePlayers(first,second);
-game.playGame(gameBoard.takeInput);
+game.playGame(gameBoard.takeInput,gameBoard.board); 
